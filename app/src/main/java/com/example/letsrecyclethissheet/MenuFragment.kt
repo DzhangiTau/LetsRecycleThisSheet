50c81cec2cd31c95548633ad94773f736a7dafc0
pkonstantinov <pavelgelazarishvili@gmail.com>
2022-11-27 15:57:55 +0400

    Handle clicks in fragment

--- app/src/main/java/com/example/letsrecyclethissheet/MenuFragment.kt
+++ app/src/main/java/com/example/letsrecyclethissheet/MenuFragment.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -23,11 +26,24 @@ class MenuFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val adapter = MenuItemAdapter(Utils.generateMenuItems(100))
+        adapter.setOnItemClickListener(object : OnItemClickListener {
+            override fun onItemClick(title: TextView, position: Int) {
+                Toast.makeText(requireContext(), "${title.text}", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onButtonClick(button: Button, position: Int) {
+                Toast.makeText(requireContext(), "${button.text}", Toast.LENGTH_SHORT).show()
+            }
+        })
         binding.recyclerRv.apply {
             setHasFixedSize(true)
             this.adapter = adapter
             layoutManager = LinearLayoutManager(requireContext())
 
         }
+    }
+
+    private fun initAdapter() {
+
     }
 }
--- app/src/main/java/com/example/letsrecyclethissheet/MenuItemAdapter.kt
+++ app/src/main/java/com/example/letsrecyclethissheet/MenuItemAdapter.kt
@@ -2,21 +2,34 @@ package com.example.letsrecyclethissheet
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class MenuItemAdapter(private val items: List<Pair<String, String>>) :
     RecyclerView.Adapter<MenuItemViewHolder>() {
+    private lateinit var listener: OnItemClickListener
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MenuItemViewHolder {
         val context = parent.context
         val inflater = LayoutInflater.from(context)
         val itemView = inflater.inflate(R.layout.menu_item, parent, false)
-        return MenuItemViewHolder(context, itemView)
+        return MenuItemViewHolder(itemView)
     }
 
     override fun onBindViewHolder(holder: MenuItemViewHolder, position: Int) {
         val item = items[position]
-        holder.bind(item)
+        holder.bind(item, listener, position)
     }
 
     override fun getItemCount(): Int = items.size
+
+    fun setOnItemClickListener(listener: OnItemClickListener) {
+        this.listener = listener
+    }
+}
+
+interface OnItemClickListener {
+    fun onItemClick(title: TextView, position: Int)
+    fun onButtonClick(button: Button, position: Int)
 }
